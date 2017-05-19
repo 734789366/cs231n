7ae46ee7b267c930a2f332c2a826428f68c20eba
Qiu Peiyang <734789366@qq.com>
2017-05-19 18:07:13 +0800

    tensorflow: add logging and monitoring components
    Add logging and monitoring units for IRIS case.
    Add ValidationMonitor to the classifier and also
    add Metrics to record more specific information.
    
    Signed-off-by: Qiu Peiyang <734789366@qq.com>

--- iris_dnn.py
+++ iris_dnn.py
@@ -14,7 +14,17 @@ Flower species are represented as integers, with 0 denoting Iris setosa,
 A training set of 120 samples
 A test set of 30 samples
 '''
-
+'''
+For the iris neural network classifier, while logging training loss, you might
+also want to simultaneously evaluate against test data to see how well the
+model is generalizing. You can accomplish this by configuring a
+ValidationMonitor with the test data (test_set.data and test_set.target),
+and setting how often to evaluate with every_n_steps.
+ValidationMonitors rely on saved checkpoints to perform evaluation operations,
+so you'll want to modify instantiation of the classifier to add a
+tf.contrib.learn.RunConfig that includes save_checkpoints_secs, which
+specifies how many seconds should elapse between checkpoint saves during training
+'''
 import numpy as np
 import tensorflow as tf
 import os
@@ -61,8 +71,45 @@ def main():
 #dimension must be set to 4 to hold all the data.
 
     feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
+#By default, if no evaluation metrics are specified, ValidationMonitor will
+#log both loss and accuracy, but you can customize the list of metrics that
+#will be run every 50 steps. To specify the exact metrics you'd like to run
+#in each evaluation pass, you can add a metrics param to the ValidationMonitor
+#constructor. metrics takes a dict of key/value pairs, where each key is the
+#name you'd like logged for the metric, and the corresponding value is a
+#MetricSpec object.
+# metric_fn. The function that calculates and returns the value of a metric.
+# prediction_key. The key of the tensor containing the predictions returned by the model
+# label_key. The key of the tensor containing the labels returned by the model,
+# as specified by the model's input_fn
+# weights_key. Optional. The key of the tensor (returned by the input_fn)
+# containing weights inputs for the metric_fn
+    validation_metrics = {
+        "accuracy":
+            tf.contrib.learn.MetricSpec(
+                metric_fn=tf.contrib.metrics.streaming_accuracy,
+                prediction_key=tf.contrib.learn.PredictionKey.CLASSES),
+        "precision":
+            tf.contrib.learn.MetricSpec(
+                metric_fn=tf.contrib.metrics.streaming_precision,
+                prediction_key=tf.contrib.learn.PredictionKey.CLASSES),
+        "recall":
+            tf.contrib.learn.MetricSpec(
+                metric_fn=tf.contrib.metrics.streaming_recall,
+                prediction_key=tf.contrib.learn.PredictionKey.CLASSES)
+    }
 
-
+#Note that in the above log output, by step 600, the model has already
+#achieved precision and recall rates of 1.0. This raises the question
+#as to whether model training could benefit from early stopping.
+    validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+                            test_set.data,
+                            test_set.target,
+                            every_n_steps=50,
+                            metrics=validation_metrics,
+                            early_stopping_metric='loss',
+                            early_stopping_metric_minimize=True,
+                            early_stopping_rounds=200)
 #tf.contrib.learn offers a variety of predefined models, called Estimators,
 #which you can use "out of the box" to run training and evaluation operations
 #on your data. Here, you'll configure a Deep Neural Network Classifier model
@@ -72,7 +119,8 @@ def main():
     classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                                 hidden_units=(10, 20, 10),
                                                 n_classes=3,
-                                                model_dir="./iris_model")
+                                                model_dir="./iris_model",
+                                                config=tf.contrib.learn.RunConfig(save_checkpoints_secs=1))
 
 
 #Datasets in tf.contrib.learn are named tuples; you can access feature data and
@@ -95,7 +143,7 @@ def main():
 #Iris training data using the fit method. Pass get_train_inputs as the input_fn,
 #and the number of steps to train (here, 2000):
 
-    classifier.fit(input_fn=get_train_inputs, steps=2000)
+    classifier.fit(input_fn=get_train_inputs, steps=2000, monitors=[validation_monitor])
     accuracy = classifier.evaluate(input_fn=get_test_inputs, steps=1)['accuracy']
     print ("Evaluate accuracy: %g" % accuracy)
 
